b05029cf82327a841ff6a7da58304051cda9eed0
Spencer <robinsonspencerm@gmail.com>
2018-09-13 17:33:08 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Home from '../src/components/home/Home';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Home></Home>
+      </div>
+    );
+  }
+}
+
+export default App;
