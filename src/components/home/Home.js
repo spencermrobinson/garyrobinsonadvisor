d993cc086e812ab14e0d2c6abb15de18b6fda5b8
Spencer <robinsonspencerm@gmail.com>
2018-09-18 16:28:22 -0600

    home-page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from '../header/Header.js';
+import Lax from './Lax-Section/Lax.js'
 import './home.css';
 
 class Home extends Component{
@@ -7,7 +8,16 @@ class Home extends Component{
     render(){
         return(
             <div className="home-page" id="background">
-             <Header className="header"/>
+                <div className="home-header-container">
+                    <Header/>
+                </div>
+                <div className="content">
+                    <div className="home-lax-container">
+                        <Lax/>
+                    </div>  
+                </div> 
+                
+                 
             </div> 
         )
     }
