abe27f681b6752c6c7cdb15b4a6bb0ac17bbffdb
Spencer <robinsonspencerm@gmail.com>
2018-09-17 18:14:48 -0600

    fixing hamburger menu

--- src/components/header/Hamburger.js
+++ src/components/header/Hamburger.js
@@ -8,7 +8,7 @@ class Hamburger extends Component{
     render(){
         return(
             <div className="ham-nav">
-                <label for="toggle">&#9776;</label>
+                <label for="toggle"><div className="bar" id="a"></div><div className="bar" id="b"></div><div className="bar" id="c"></div></label>
                 <input type='checkbox' id='toggle'/>
                 <div className="ham-menu">
                 <a href="">ABOUT</a>
