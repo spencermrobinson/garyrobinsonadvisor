6e4be72927f538181709e5286c8d4ce888ef2645
Spencer <robinsonspencerm@gmail.com>
2018-09-20 00:42:54 -0600

    contact header

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
+import Header from '../header/Header.js'
 
 class Contact extends Component{
 
     render(){
         return(
             <div>
-            Contact
+                <div><Header/></div>
+                <h1>THIS PAGE IS CURRENTLY BEING BUILT</h1> 
             </div> 
         )
     }
